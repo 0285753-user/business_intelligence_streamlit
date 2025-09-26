447138277af191aa47e10b2089ff5399690097fd
0285753-user <0285753@up.edu.mx>
2025-09-26 17:40:14 -0600

    Update header.py

--- Modules/UI/header.py
+++ Modules/UI/header.py
@@ -3,12 +3,12 @@
 import streamlit as st
 
 def show_header(text_title: str):
-  #layout: logo + Title side by side
+  #layout: logo + title side by side
   col1, col2 = st.columns([1, 6])
 
   with col1:
-    #st.image("assets/coffee.jpg", width=200)
-
+    st.image("assets/coffee.jpg", width=200)
+  
   with col2:
     st.title(text_title)
     st.caption("Developed for: *Business Inteligene Project*")
