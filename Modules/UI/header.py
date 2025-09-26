e6ecb3fba77ce45df095d39d0c0b6a0ac7ebc0fd
0285753-user <0285753@up.edu.mx>
2025-09-26 17:28:45 -0600

    Update header.py

--- Modules/UI/header.py
+++ Modules/UI/header.py
@@ -1,1 +1,15 @@
+#Encabezado de la interfaz grafica de la app
 
+import streamlit as st
+
+def show_header(text_title: str):
+  #layout: logo + Title side by side
+  col1, col2 = st.columns([1, 6])
+
+  with col1:
+    st.image("assets/coffee.jpg", width=200)
+
+  with col2:
+    st.title(text_title)
+    st.caption("Developed for: *Business Inteligene Project*")
+    st.caption("Developed by Team 4")
