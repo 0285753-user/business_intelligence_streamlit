a97040191f96e488d484c16fd7d91667078f7975
0285753-user <0285753@up.edu.mx>
2025-09-26 19:12:36 -0600

    Update dataframe.py

--- Modules/UI/dataframe.py
+++ Modules/UI/dataframe.py
@@ -1,1 +1,9 @@
+from Modules.Utils.add_df import create_df
+import streamlit as st
+import pandas as pd
 
+df = create_df()
+
+# Muestra el DataFrame de forma no editable
+st.subheader('DataFrame No Editable')
+st.dataframe(df)
