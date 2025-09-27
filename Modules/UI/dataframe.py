99aa0cbda72df44a9c917c6b3b80ee589866fdc7
0285753-user <0285753@up.edu.mx>
2025-09-26 19:18:56 -0600

    Update dataframe.py

--- Modules/UI/dataframe.py
+++ Modules/UI/dataframe.py
@@ -3,8 +3,12 @@ import streamlit as st
 import pandas as pd
 
 def dataframe():
+
+  url = 'https://raw.githubusercontent.com/Roby20030202/BI_FINAL_PROJECT/refs/heads/main/filtered_yelp_NJ.csv'
+
+  df = pd.read_csv(url,index_col=0)
   
-  df = create_df()
+  df = create_df(df)
   
   # Muestra el DataFrame de forma no editable
   st.subheader('DataFrame No Editable')
