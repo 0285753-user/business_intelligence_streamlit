0d40adac473a58999b7778634b75ee80dee49a0c
0285753-user <0285753@up.edu.mx>
2025-09-27 19:56:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 # Seccion de importacion de modulos
 from Modules.UI.header import show_header
 from Modules.UI.dataframe import all_data
+from Modules.UI.nj_map import map_nj
 
 # Seccion para crear la GUI
 show_header("Organic Coffee Supplier")
 
 all_data()
+
+map_nj()
