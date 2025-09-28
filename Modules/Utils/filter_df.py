e487164a3c76e6b9f138a3249f40623aeb69ba4e
0285753-user <0285753@up.edu.mx>
2025-09-27 20:30:28 -0600

    Create filter_df.py

---
+++ Modules/Utils/filter_df.py
@@ -0,0 +1,54 @@
+from Modules.Utils.add_df import coffee_data
+import pandas as pd
+
+def filtered_data():
+
+  url = 'https://raw.githubusercontent.com/Roby20030202/BI_FINAL_PROJECT/refs/heads/main/filtered_yelp_NJ.csv'
+
+  df = pd.read_csv(url,index_col=0)
+
+  df_coffee_tea = coffee_data(df)
+
+  df_coffee = df_coffee_tea.reset_index()
+  
+  df_speciality = df_coffee[~df_coffee['name'].str.contains('Starbucks', case=False)]
+  df_speciality = df_speciality[~df_speciality['name'].str.contains('Dunkin', case=False)]
+  df_speciality = df_speciality[~df_speciality['name'].str.contains('McDonald', case=False)]
+
+  def categorize(row):
+    if row['Cafes'] > 0:
+        return 'Cafes'
+    elif row['Restaurants']  > 0:
+        return 'Restaurants'
+    elif row['Breakfast & Brunch']  > 0:
+        return 'Breakfast & Brunch'
+    elif row['Bakeries']  > 0:
+        return 'Bakeries'
+    elif row['Desserts']  > 0:
+        return 'Desserts'
+    elif row['Specialty Food']  > 0:
+        return 'Specialty Food'
+    elif row['Convenience Stores']  > 0:
+        return 'Convenience Stores'
+    elif row['Coffee Roasteries']  > 0:
+        return 'Coffee Roasteries'
+    elif row['Food Trucks']  > 0:
+        return 'Food Trucks'
+    elif row['Bars']  > 0:
+        return 'Bars'
+
+    else:
+        return 'Other'
+
+    df_specialty_new['Category'] = df_speciality.apply(categorize, axis=1)
+    
+    def category(row):
+        if row['Coffee & Tea'] > 0:
+            return 'Coffee & Tea'
+    
+    df_specialty_new['Main_Category'] = df_specialty_new.apply(category, axis=1)
+    
+  
+  return df_specialty_new
+
+  
